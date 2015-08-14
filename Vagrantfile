59bcdcd94596439503a306a4aeb7706f4b4a1ae8
Matt Dailey <matthew.dailey1@gmail.com>
2015-08-13 22:26:37 -0400

    Added JDK 8 and IntelliJ to bootstrap.  Attempting to get X forwarding working properly

--- Vagrantfile
+++ Vagrantfile
@@ -14,6 +14,10 @@ Vagrant.configure(2) do |config|
   # boxes at https://atlas.hashicorp.com/search.
   config.vm.box = "chef/centos-7.0"
 
+  # ssh properties
+  config.ssh.forward_agent = true
+  config.ssh.forward_x11 = true
+
   # Disable automatic box update checking. If you disable this, then
   # boxes will only be checked for updates when the user runs
   # `vagrant box outdated`. This is not recommended.
--- bootstrap.sh
+++ bootstrap.sh
@@ -2,3 +2,40 @@
 
 sudo yum update
 sudo yum install -y gcc-c++ git
+
+# pull down Java and install it
+JDK_FILE=jdk-8u51-linux-x64.rpm
+JDK_URL=http://download.oracle.com/otn-pub/java/jdk/8u51-b16/${JDK_FILE}
+
+if [ -e /vagrant/$JDK_FILE ] ; then
+    echo "Copying $JDK_FILE"
+    cp /vagrant/$JDK_FILE .
+else
+    echo "Downloading $JDK_FILE"
+    wget --no-check-certificate --no-cookies \
+         --header "Cookie: oraclelicense=accept-securebackup-cookie" \
+         $JDK_URL
+    cp $JDK_FILE /vagrant/$JDK_FILE
+fi
+sudo rpm -ivh $JDK_FILE
+
+# install intellij community edition and put in path
+IDEA_FILE=ideaIC-14.1.4.tar.gz
+
+if [ -e /vagrant/$IDEA_FILE ] ; then
+    echo "Extracting $IDEA_FILE"
+    tar xzvf /vagrant/$IDEA_FILE
+else
+    echo "Downloading $IDEA_FILE"
+    wget http://download.jetbrains.com/idea/$IDEA_FILE
+    cp $IDEA_FILE /vagrant/$IDEA_FILE
+    tar xzvf $IDEA_FILE
+fi
+ln -sf idea-IC-* idea
+
+echo '
+export PATH=$PATH:$HOME/idea/bin
+' >> $HOME/.bash_profile
+
+# cleanup
+#rm -f $IDEA_FILE $JDK_FILE
