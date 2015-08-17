4a40d941a955445df0c3047a2b0af2b1bf376f74
Matt Dailey <matthew.dailey1@gmail.com>
2015-08-16 22:45:10 -0400

    Moved download processing to Chef script

--- intellij-java/bootstrap.sh
+++ intellij-java/bootstrap.sh
@@ -16,39 +16,20 @@ rpm -ivh /vagrant/$CHEF_FILE
 
 # TODO: should put this as a 'provision' in the Vagrantfile
 chef-apply /vagrant/packages.rb
+chef-apply /vagrant/manual-packages.rb
 
 # pull down Java and install it
 JDK_FILE=jdk-8u51-linux-x64.rpm
-JDK_URL=http://download.oracle.com/otn-pub/java/jdk/8u51-b16/${JDK_FILE}
-
-if ! [ -e /vagrant/$JDK_FILE ] ; then
-    echo "Downloading $JDK_FILE"
-    wget --no-check-certificate --no-cookies \
-         --header "Cookie: oraclelicense=accept-securebackup-cookie" \
-         $JDK_URL
-    mv $JDK_FILE /vagrant/$JDK_FILE
-fi
 sudo rpm -ivh /vagrant/$JDK_FILE
 
 # install intellij community edition and put in path
 IDEA_FILE=ideaIC-14.1.4.tar.gz
-
-if ! [ -e /vagrant/$IDEA_FILE ] ; then
-    echo "Downloading $IDEA_FILE"
-    wget http://download.jetbrains.com/idea/$IDEA_FILE
-    mv $IDEA_FILE /vagrant/$IDEA_FILE
-fi
 tar xzvf /vagrant/$IDEA_FILE
 chown -R vagrant:vagrant idea-IC-*
 ln -sf idea-IC-* idea
 
 # install Maven
 MAVEN_FILE=apache-maven-3.3.3-bin.tar.gz
-if ! [ -e /vagrant/$MAVEN_FILE ] ; then
-    echo "Downloading $MAVEN_FILE"
-    wget http://mirror.reverse.net/pub/apache/maven/maven-3/3.3.3/binaries/$MAVEN_FILE
-    mv $MAVEN_FILE /vagrant/$MAVEN_FILE
-fi
 tar xzvf /vagrant/$MAVEN_FILE
 chown -R vagrant:vagrant apache-maven-*
 ln -sf apache-maven-* apache-maven
--- intellij-java/manual-packages.rb
+++ intellij-java/manual-packages.rb
@@ -1,9 +1,25 @@
 # Chef script for manually installing tar/RPM files from remote
 
-IDEA_FILE=ideaIC-14.1.4.tar.gz
+IDEA_FILE = 'ideaIC-14.1.4.tar.gz'
+JDK_FILE = 'jdk-8u51-linux-x64.rpm'
+MAVEN_FILE = 'apache-maven-3.3.3-bin.tar.gz'
 
-remote_file "/vagrant/#{IDEA_FILE}" do
-    source "http://download.jetbrains.com/idea/#{IDEA_FILE}"
-    mode 0644
-    checksum '3a8dc4a12ab9f3607a1a2097bbab0150c947ad6719d8f1bb6d5b47d0fb0c4779'
+# path, url, sha2
+pkgs = [
+    ["/vagrant/#{IDEA_FILE}", "http://download.jetbrains.com/idea/#{IDEA_FILE}", 'bd13fdd487ff37fb8b1d6aa713a6ff2c642b8b2ed74d292a8b624ab55c1ba9fb'],
+    ["/vagrant/#{JDK_FILE}", "http://download.oracle.com/otn-pub/java/jdk/8u51-b16/#{JDK_FILE}", '49527b54d8e179c8da3547fbac223e9b1ecb4b5276b47823c466617aec4bb97b'],
+    ["/vagrant/#{MAVEN_FILE}", "http://mirror.reverse.net/pub/apache/maven/maven-3/3.3.3/binaries/#{MAVEN_FILE}", '3a8dc4a12ab9f3607a1a2097bbab0150c947ad6719d8f1bb6d5b47d0fb0c4779']
+]
+
+# TODO: wget needed special cookies to download the JDK.  Can we do that here?
+pkgs.each do |pkg|
+    path, url, sha2 = pkg
+    remote_file path do
+        source url
+        checksum sha2
+        mode 0644
+    end
 end
+
+# TODO: install rpms and untar the tgz files
+# TODO: add idea.sh to path
