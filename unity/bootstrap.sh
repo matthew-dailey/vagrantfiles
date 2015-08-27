4540dab85b9f81f3150dacfa07557b865fd91023
Matt Dailey <matthew.dailey1@gmail.com>
2015-08-26 21:45:15 -0400

    unity bootstrap sets up PATH

--- unity/bootstrap.sh
+++ unity/bootstrap.sh
@@ -14,11 +14,20 @@ if ! [ -e /vagrant/$UNITY_FILE ] ; then
     wget $UNITY_URL
     mv $UNITY_FILE /vagrant/$UNITY_FILE
 fi
+
 sudo dpkg -i /vagrant/$UNITY_FILE
-sudo apt-get install -f -y
+if ! sudo apt-get install -f -y ; then
+    echo "Failed to install unity .deb file"
+    exit 1
+fi
 
 # Add 'Unity' and 'monodevelop' to PATH
 echo '
-mono_home=/opt/Unity/MonoDevelop
+mono_home=/opt/Unity/Editor/Data/Mono
+monodev_home=/opt/Unity/MonoDevelop
 unity_home=/opt/Unity/Editor
-export PATH=$PATH:${mono_home}/bin:${unity_home}'
+export PATH=$PATH:${mono_home}/bin:${monodev_home}/bin
+
+# Unity likes to be called with full path
+alias Unity="${unity_home}/Unity"
+' >> ~vagrant/.bash_profile
