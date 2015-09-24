7c9d7c2bdce038f5e59f8018d59fb669cdfc842f
Matt Dailey <matthew.dailey1@gmail.com>
2015-09-23 22:16:53 -0400

    pared down old-cpp yum deps

--- old-cpp/bootstrap.sh
+++ old-cpp/bootstrap.sh
@@ -1,25 +1,8 @@
 #!/bin/bash
 
-folly_deps='
+cpp_deps='
 g++
-automake
-autoconf
-autoconf-archive
-libtool
-libboost-all-dev
-libevent-dev
-libdouble-conversion-dev
-libgoogle-glog-dev
-libgflags-dev
-liblz4-dev
-liblzma-dev
-libsnappy-dev
 make
-zlib1g-dev
-binutils-dev
-libjemalloc-dev
-libssl-dev
-libiberty-dev
 '
 
 devtools='
@@ -31,26 +14,7 @@ unzip
 set -e
 
 # install dependencies
-sudo apt-get update
-sudo apt-get install -y \
-    $folly_deps \
+sudo yum update -y
+sudo yum install -y \
+    $cpp_deps \
     $devtools
-
-# setup workspace
-sudo mkdir -p /workspace 
-sudo chown vagrant:vagrant /workspace
-
-# clone folly
-cd /workspace && git clone https://github.com/facebook/folly.git
-
-# extract gtest into folly's test dir
-wget https://googletest.googlecode.com/files/gtest-1.7.0.zip -O /workspace/gtest-1.7.0.zip
-unzip /workspace/gtest-1.7.0.zip -d /workspace/folly/folly/test/
-
-# build, test, install
-cd /workspace/folly/folly && \
-    autoreconf -ivf && \
-    ./configure && \
-    make && \
-    make check && \
-    sudo make install
